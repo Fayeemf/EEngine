44b0c5b564f69f2382ab8ed7ef7a1a5e9483d0fa
val92130 <val_92_@hotmail.fr>
2016-11-14 21:46:06 +0100

    Fixed examples

--- build/ee.js
+++ build/ee.js
@@ -1476,4 +1476,3 @@ EE.GameHost = function(gameWidth, gameHeight) {
   this._construct(new EE.NullRenderSurface(1280, 1200), {});
 };
 
-EE.GameHost.prototype = new EE.Game(new EE.NullRenderSurface(1280, 1200), {});
--- src/core/GameHost.js
+++ src/core/GameHost.js
@@ -2,4 +2,3 @@ EE.GameHost = function(gameWidth, gameHeight) {
   this._construct(new EE.NullRenderSurface(1280, 1200), {});
 };
 
-EE.GameHost.prototype = new EE.Game(new EE.NullRenderSurface(1280, 1200), {});
